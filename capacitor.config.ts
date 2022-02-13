633ba62068f5e32c65b8e44f981126cc327cf118
Elvis Duru <duruelvis95@gmail.com>
2022-02-13 14:43:39 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'cheqq-app',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
