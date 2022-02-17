36f2325712b615e0cf4648b198e39b23a1599f99
Elvis Duru <duruelvis95@gmail.com>
2022-02-17 03:52:46 +0100

    Started project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'cheqq-app',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "co.cheqq.cheqq",
+  appName: "cheqq-app",
+  webDir: "build",
+  bundledWebRuntime: false,
 };
 
 export default config;
