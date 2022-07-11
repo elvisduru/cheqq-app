5c7d54048526456d595c96ce126bd733cc3904a8
Elvis Duru <duruelvis95@gmail.com>
2022-07-11 20:32:36 +0100

    Add: vite pwa

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,6 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "react-query";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const queryClient = new QueryClient();
 
@@ -34,11 +33,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 
 // Call the element loader after the app has been rendered the first time
 defineCustomElements(window);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,59 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*{js,css,html,ico,png,svg,json}"],
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        short_name: "Cheqq",
+        name: "Cheqq",
+        icons: [
+          {
+            src: "favicon.ico",
+            type: "image/x-icon",
+            sizes: "32x32",
+          },
+          {
+            src: "icon-192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "icon-512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+          {
+            src: "icon-192-maskable.png",
+            type: "image/png",
+            sizes: "192x192",
+            purpose: "maskable",
+          },
+          {
+            src: "icon-512-maskable.png",
+            type: "image/png",
+            sizes: "512x512",
+            purpose: "maskable",
+          },
+        ],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#000000",
+        background_color: "#000000",
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   server: {
     port: 8100,
     host: true,
